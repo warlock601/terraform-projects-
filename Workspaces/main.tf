ef0222d86a6bafcfb87a0f7627b204c960078ea2
vivek <vr32288@gmail.com>
2024-04-10 07:23:15 +0530

    a

--- Workspaces/main.tf
+++ Workspaces/main.tf
@@ -1,1 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+variable "ami" {
+    description = "to specify AMI value"
+}
+
+variable "instance_type" {
+    description = "to specify instance type"
+  
+}
+resource "aws_instance" "name" {
+    ami = var.ami
+    instance_type = var.instance_type
+
+}
