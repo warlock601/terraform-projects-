a9cfc6d6e639f4428973cbd9c764244a666c8c49
Vivek Raj <vr32288@gmail.com>
2024-04-11 18:14:43 +0530

    example alias main.tf created

--- Alias/main.tf
+++ Alias/main.tf
@@ -1,1 +1,21 @@
+provider "aws" {                          // created alias A for region us-east
+  region = "us-east-1"
+  alias = "A"
+}
 
+provider "aws" {                          // created alias B for region us-west
+  region = "us-west-1"
+  alias = "B"
+}
+
+resource "aws_instance" "vivek-ec2" {
+  ami = ""
+  instance_type = ""
+  provider = aws.A                             // created with alias B which refers to region us-east
+}
+
+resource "aws_instance" "vivek-ec2" {           
+  ami = ""
+  instance_type = ""
+  provider = aws.B                             // created with alias B which refers to region us-west 
+}
