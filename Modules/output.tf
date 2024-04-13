7395254befbea0b6f99935fa484c097bc549d5d6
Vivek Raj <vr32288@gmail.com>
2024-04-13 14:42:55 +0530

    added code output.tf

--- Modules/output.tf
+++ Modules/output.tf
@@ -1,1 +1,3 @@
-
+output "ip_address" {                                             
+  value = aws_instance.example.public_ip                            // specify values that we wanna get as outputs.
+}
