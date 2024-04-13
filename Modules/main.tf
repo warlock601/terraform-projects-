3fc5e2887114f9aa7d1750e3a439a4300dc8efcb
Vivek Raj <vr32288@gmail.com>
2024-04-13 14:42:08 +0530

    added terraform code to main.tf

--- Modules/main.tf
+++ Modules/main.tf
@@ -1,1 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_instance" "example" {
+  ami = var.ami
+  instance_type = var.instance_type
+}
+
+/*
+
+module "ec2_instance" {                            // we can use module to provide path where we've stored our modules and even github URL 
+  source = "value"
+}
+
+*/
